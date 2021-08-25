e1b9f33ed6f87ed4281ce506ccda56978d57bc57
Jeysen Freedman <jeysenfreedman@gmail.com>
2021-08-25 11:26:41 -0400

    add homepage image

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { HomePageContainer } from './homepage.styles';
+import { HomePageContainer, HomePageBackgroundImage } from './homepage.styles';
 import { StyledH1, StyledList } from '../../generalstyles';
 
 import Directory from '../../components/directory/directory.component.jsx';
@@ -8,23 +8,25 @@ import Directory from '../../components/directory/directory.component.jsx';
 const HomePage = () => {
   return (
     <HomePageContainer>
-      <StyledH1>
-        Sustainably grown local produce - and other natural stuff!
-      </StyledH1>
-      <StyledList>
-        <li>Welcome to the home of your freshest locally grown produce!</li>
-      </StyledList>
-      <StyledList>
-        <li>
-          Browse our selection and place your order - no sign up required!
-        </li>
-        <li>
-          <small>
-            Create an account to see your past orders and get access to
-            exclusive offers
-          </small>
-        </li>
-      </StyledList>
+      <HomePageBackgroundImage>
+        <StyledH1>
+          Sustainably grown local produce - and other natural stuff!
+        </StyledH1>
+        <StyledList>
+          <li>Welcome to the home of your freshest locally grown produce!</li>
+        </StyledList>
+        <StyledList>
+          <li>
+            Browse our selection and place your order - no sign up required!
+          </li>
+          <li>
+            <small>
+              Create an account to see your past orders and get access to
+              exclusive offers
+            </small>
+          </li>
+        </StyledList>
+      </HomePageBackgroundImage>
       <Directory />
     </HomePageContainer>
   );
--- client/src/pages/homepage/homepage.styles.jsx
+++ client/src/pages/homepage/homepage.styles.jsx
@@ -9,3 +9,23 @@ export const HomePageContainer = styled.section`
     padding: 10px;
   }
 `;
+
+export const HomePageBackgroundImage = styled.section`
+  height: 75vh;
+  width: 100%;
+  background-image: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0.6),
+      rgba(0, 0, 0, 0.6)
+    ),
+    url(https://res.cloudinary.com/dnpfrwpiq/image/upload/v1629904972/carrotcart/homepage-image-1_twkmht.jpg);
+  background-position: center;
+  background-size: cover;
+  padding: 20px 40px;
+  border-radius: 10px;
+  box-shadow: 0 5px 8px rgba(0, 0, 0, 0.25);
+
+  @media screen and (max-width: 600px) {
+    padding: 10px 20px;
+  }
+`;
